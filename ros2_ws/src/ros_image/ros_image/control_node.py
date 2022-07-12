3875e954b01f80abd50eecf33d878c61c3d61ac9
HyeokJin Kwon <ineogi2@gmail.com>
2022-07-12 22:40:49 +0900

    0712 update

--- ros2_ws/src/ros_image/ros_image/control_node.py
+++ ros2_ws/src/ros_image/ros_image/control_node.py
@@ -2,14 +2,15 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 import cv2 as cv
 import numpy as np
 import sys
 
+# ------Class------
 class Imagenode(Node):
 
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__('image_node')           ### 노드 이름
         qos_profile = QoSProfile(depth=10)       ### 버퍼 설정
 
@@ -18,11 +19,10 @@ class Imagenode(Node):
         self.cv_bridge = CvBridge()
 
         # mode 1 : hsv / mode 2 : canny
-        self.mode = 2
+        self.mode = mode
 
 
-# ------callback------
-
+    # ------callback------
     def subscribe_pic(self, img):
 
         image = self.cv_bridge.imgmsg_to_cv2(img, desired_encoding="bgr8")
@@ -36,7 +36,7 @@ class Imagenode(Node):
         cv.destroyAllWindows()
 
 
-# ------vision function------
+    # ------vision function------
     def nothing(self, x):
         pass
 
@@ -82,32 +82,46 @@ class Imagenode(Node):
                 break
 
     def change_canny(self, image):
-        # Create trackbars for change
+        # Create trackbars for change canny threshold
         cv.createTrackbar('Low', 'image', 0, 1000, self.nothing)
         cv.createTrackbar('High', 'image', 0, 1000, self.nothing)
+        cv.createTrackbar('MinLineLength', 'image', 0, 300, self.nothing)
+        cv.createTrackbar('MaxLineGap', 'image', 0, 10, self.nothing)
 
-        # Set default value for Max canny trackbars
+        # Set default threshold for canny trackbars
         cv.setTrackbarPos('Low', 'image', 50)
         cv.setTrackbarPos('High', 'image', 150)
+        cv.setTrackbarPos('MinLineLength', 'image', 50)
+        cv.setTrackbarPos('MaxLineGap', 'image', 2)
 
         while(1):
             # Get current positions of all trackbars
             low = cv.getTrackbarPos('Low', 'image')
             high = cv.getTrackbarPos('High', 'image')
+            length = cv.getTrackbarPos('MinLineLength', 'image')
+            gap = cv.getTrackbarPos('MaxLineGap', 'image')
 
-            # Convert to HSV format and color threshold
+            # Do canny function
             img_canny = cv.Canny(image, low, high)
+            lines = cv.HoughLinesP(img_canny, 1, np.pi/180., 120, minLineLength=length, maxLineGap=gap)
+            dst = cv.cvtColor(img_canny, cv.COLOR_GRAY2BGR)
+
+            if lines is not None:
+                for i in range(lines.shape[0]):
+                    pt1 = (lines[i][0][0], lines[i][0][1])
+                    pt2 = (lines[i][0][2], lines[i][0][3])
+                    cv.line(dst, pt1, pt2, (0,0,255), 2, cv.LINE_AA)
 
             # Display result image
-            cv.imshow('image', img_canny)
+            cv.imshow('image', dst)
             if cv.waitKey(10) & 0xFF == ord('q'):
                 self.get_logger().info(f'Low : {low} / High : {high}')
                 break
 
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = Imagenode()
+# ------main function------
+def main():
+    rclpy.init()
+    node = Imagenode(sys.argv[1])
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -116,6 +130,6 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
-
+# ------execution------
 if __name__ == '__main__':
     main()
